e6e288abaea2f7467f81ecff9819e84253adeb3f
tegshee <tegshbayar@gmail.com>
2020-05-18 13:36:28 -0700

    Complete and pass tests for Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor (age) {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
+
 };
